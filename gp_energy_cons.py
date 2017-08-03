287534a8988338eea19321eefc033e686943eefa
Spencer Purves <spencer.purves31@gmail.com>
2017-08-02 20:21:28 -0600

    update HMM_GP and gp_energy_cons.py

--- gp_energy_cons.py
+++ gp_energy_cons.py
@@ -79,9 +79,10 @@ plot_y(train_Y, 'All of train_Y', 'Hours', 'Electric load (normalized)')
 plot_y(train_Y[0:168], 'First week of train_Y', 'Hours', 'Electric load (normalized)', 'Week 1')
 
 # Density plot of the response variable, i.e. the distribution
-sns.distplot(train_Y, norm_hist=True, color = 'blue')
-sns.plt.suptitle('Density of train_Y')
-sns.plt.show()
+plt.figure(figsize=(12, 8))
+g = sns.distplot(train_Y, norm_hist=True, color = 'blue')
+g.figure.suptitle('Density of train_Y')
+
 
 # Correlation analysis on features - doesn't appear to have any significant correlations
 corrs = pd.DataFrame(train_X)
@@ -105,52 +106,89 @@ sns.heatmap(corrs, xticklabels=np.arange(12), yticklabels=np.arange(12))
 
 # GP on entire training dataset withouth HMM
 kernel = ConstantKernel() + RationalQuadratic() + WhiteKernel()
-gp = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5)
-gp_model = gp.fit(train_X, train_Y)
-joblib.dump(gp_model, 'C:/Users/spenc/Documents/COURSES/CS6961_StructuredPrediction/Energy_Consumption/GP_model.pkl') 
-gp_model.score(train_X, train_Y)
-print rmse(gp_model.predict(train_X), train_Y)
 
+# Instantiate and fit the model
+gp_model = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(train_X, train_Y)
+
+# Save the model to a file for future reference
+#joblib.dump(gp_model, 'C:/Users/spenc/Documents/COURSES/CS6961_StructuredPrediction/Energy_Consumption/GP_model.pkl') 
+# to load model run
+#gp_model = joblib.load('GP_model.pkl') 
+
+# Get predictions and standard deviations of the model on train_X
+train_pred, train_std = gp_model.predict(train_X, return_std=True)
+
+#print the r-squared ad rmse of the predictions on training data
+print 'R-squared: ', gp_model.score(train_X, train_Y)
+print 'RMSE: ', rmse(train_pred, train_Y)
+
+# Plot the true values of train_Y vs model predictions on train_Y
+plot_y([train_Y, train_pred],  'True values vs. model predictions on test data', 'Hours', 'Electric load (normalized)', ['Truth','Prediction'])
+in_ci = plot_CI('Confidence Interval on training data', train_Y, train_pred, train_std, 95, 250, 450)
+print 'The percent of true values that fall within the confidence interval is ', 100*in_ci,'%'
+
+# Density plot of the test response variable and the predicted values
+plt.figure(figsize=(12, 8))
+sns.distplot(train_Y, norm_hist=True, color = 'blue', label = 'True')
+sns.distplot(train_pred, norm_hist=True, color = 'red', label = 'Predicted')
+plt.suptitle('Density of true vs. predicted values on training data')
+plt.legend()
+plt.show()
+
+# get the predictions and standard deviation of test data
 test_pred, test_std = gp_model.predict(test_X, return_std=True)
 
+# Save the predictions and std deviation
 np.save('C:/Users/spenc/Documents/COURSES/CS6961_StructuredPrediction/Energy_Consumption/GP_test_pred', test_pred)
 np.save('C:/Users/spenc/Documents/COURSES/CS6961_StructuredPrediction/Energy_Consumption/GP_test_std', test_std)
-gp_model.score(test_X, test_Y)
-print rmse(test_pred, test_Y)
-upper = test_pred.flatten() + 1.96*test_std
-lower = test_pred.flatten() - 1.96*test_std
 
-rmse(test_pred, test_Y)
-rmse(test_pred, test_Y) / np.mean(test_Y)
-rmse(test_pred, test_Y) /(np.max(test_Y) - np.min(test_Y))
+#print the r-squared ad rmse
+print 'R-squared: ', gp_model.score(test_X, test_Y)
+print 'RMSE: ', rmse(test_pred, test_Y)
 
+# View plots of the true test_Y values vs the predictions 
+plot_y([test_Y, test_pred],  'True values vs. model predictions on unseen (test_X) data', 'Hours', 'Electric load (normalized)', ['Truth','Prediction'])
+in_ci = plot_CI('Confidence Interval on unseen (test_X) data', test_Y, test_pred, test_std, 95, 200, 325)
+print 'The percent of true values that fall within the confidence interval is ', 100*in_ci,'%'
 
-#plt.plot(test_Y[0:368], c='red')
-#plt.plot(test_pred[0:368], c='blue')   
-plot_CI(test_Y, test_pred, 95, 100)
+# Density plot of the test response variable and the predicted values
+plt.figure(figsize=(12, 8))
+sns.distplot(test_Y, norm_hist=True, color = 'blue', label = 'True')
+sns.distplot(test_pred, norm_hist=True, color = 'red', label = 'Predicted')
+plt.suptitle('Density of true vs. predicted values on unseen data')
+plt.legend()
+plt.show()
+
+######
+######  Hidden Markov with GP regression model  ##################################################
+######
 
 # PCA analysis for visual commparison of hidden states produced
 sklearn_pca = sklearnPCA()
 train_pca = sklearn_pca.fit_transform(train_X)
 
+# plot the first 3 components 
 plot_3d(train_pca)
 
 # HMM model
 hmm = GaussianHMM(n_components=7, min_covar=0.00001, tol = 0.001, n_iter=100).fit(train_X)
 
 # Predict the optimal sequence of hidden states for training data
-train_states = hmm.predict(train_X)
+train_hidden_states = hmm.predict(train_X)
 cols = ['r', 'b', 'g', 'y', 'k', 'c', 'm', 'darkgreen']
 colors = [cols[x] for x in train_states]
 
-
-zeros = train_X[np.where(train_states == 0)]
-zero_Y = train_Y[np.where(train_states == 0)]
-
-
-# 3D plot to visualize the predicted states 
+# 3D plot of the training data colored with the predicted hidden state for each data point.
+print '3D plot of the first 3 PCA components colored by hidden state that produced the data point:'
 plot_3d(train_pca, colors)
-       
+
+
+print 'Plot of train_Y, the response variable, colored by the hidden state'
+# plot the train_X data with the hidden states color coded
+plt.figure(figsize=(12, 8))
+plt.plot(train_Y[0:200])
+plt.scatter(np.arange(200), train_Y[0:200], c=colors)
+plt.show()      
 
 # Implement Gaussian Process for each state, after cross validation on different kernel combinations the
 # best kernel for each state was the ConstantKernel() + RationalQuadratic() + WhiteKernel(). The kernel
@@ -158,77 +196,64 @@ plot_3d(train_pca, colors)
 #kernel = ConstantKernel() + RationalQuadratic() + WhiteKernel()
 #gp = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = False, n_restarts_optimizer = 5)
 
+def r2_rmse(model, state):
+    print ' GP for Hidden state ', state
+    print 'R-squared: ', model.score(train_X[np.where(train_hidden_states == state-1)], train_Y[np.where(train_hidden_states == state-1)])
+    print 'RMSE: ', rmse(model.predict(train_X[np.where(train_hidden_states == state-1)]), train_Y[np.where(train_hidden_states == state-1)])
+
 models = []
 for i in np.arange(7):
     print 'Hidden state ', i
     models.append(gp.fit(train_X[np.where(train_states == i)], train_Yshuffled[np.where(train_states == i)]))
 
 
-idx = train_X[np.where(train_states == 0)]
-gp0 = gp.fit(train_X[np.where(train_states == 0)], train_Yshuffled[np.where(train_states == 0)])
-print 'R2 ', gp0.score(train_X[np.where(train_states == 0)], train_Yshuffled[np.where(train_states == 0)])
-print rmse(gp0.predict(train_X[np.where(train_states == 0)]), train_Yshuffled[np.where(train_states == 0)])
+gp_hmm0 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 0)], train_Y[np.where(train_hidden_states == 0)])
+gp_hmm1 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 1)], train_Y[np.where(train_hidden_states == 1)])
+gp_hmm2 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 2)], train_Y[np.where(train_hidden_states == 2)])
+gp_hmm3 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 3)], train_Y[np.where(train_hidden_states == 3)])
+gp_hmm4 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 4)], train_Y[np.where(train_hidden_states == 4)])
+gp_hmm5 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 5)], train_Y[np.where(train_hidden_states == 5)])
+gp_hmm6 = gaussian_process.GaussianProcessRegressor(kernel=kernel, normalize_y = True, n_restarts_optimizer = 5).fit(
+    train_X[np.where(train_hidden_states == 6)], train_Y[np.where(train_hidden_states == 6)])
 
-gp1 = gp.fit(train_X[np.where(train_states == 1)], train_Yshuffled[np.where(train_states == 1)])
-print 'R2 ', gp1.score(train_X[np.where(train_states == 1)], train_Yshuffled[np.where(train_states == 1)])
-print rmse(gp1.predict(train_X[np.where(train_states == 1)]), train_Yshuffled[np.where(train_states == 1)])
+# dictionary of hidden state and corresponding GP model
+models = {1:gp_hmm0, 2:gp_hmm1, 3:gp_hmm2, 4:gp_hmm3, 5:gp_hmm4, 6:gp_hmm5, 7:gp_hmm6}
 
-gp2 = gp.fit(train_X[np.where(train_states == 2)], train_Yshuffled[np.where(train_states == 2)])
-print 'R2 ', gp2.score(train_X[np.where(train_states == 2)], train_Yshuffled[np.where(train_states == 2)])
-print rmse(gp2.predict(train_X[np.where(train_states == 2)]), train_Yshuffled[np.where(train_states == 2)])
-
-gp3 = gp.fit(train_X[np.where(train_states == 3)], train_Yshuffled[np.where(train_states == 3)])
-print 'R2 ', gp3.score(train_X[np.where(train_states == 3)], train_Yshuffled[np.where(train_states == 3)])
-print rmse(gp3.predict(train_X[np.where(train_states == 3)]), train_Yshuffled[np.where(train_states == 3)])
-
-gp4 = gp.fit(train_X[np.where(train_states == 4)], train_Yshuffled[np.where(train_states == 4)])
-print 'R2 ', gp4.score(train_X[np.where(train_states == 4)], train_Yshuffled[np.where(train_states == 4)])
-print rmse(gp4.predict(train_X[np.where(train_states == 4)]), train_Yshuffled[np.where(train_states == 4)])
-
-gp5 = gp.fit(train_X[np.where(train_states == 5)], train_Yshuffled[np.where(train_states == 5)])
-print 'R2 ', gp5.score(train_X[np.where(train_states == 5)], train_Yshuffled[np.where(train_states == 5)])
-print rmse(gp5.predict(train_X[np.where(train_states == 5)]), train_Yshuffled[np.where(train_states == 5)])
-
-gp6 = gp.fit(train_X[np.where(train_states == 6)], train_Yshuffled[np.where(train_states == 6)])
-print 'R2 ', gp6.score(train_X[np.where(train_states == 6)], train_Yshuffled[np.where(train_states == 6)])
-print rmse(gp6.predict(train_X[np.where(train_states == 6)]), train_Yshuffled[np.where(train_states == 6)])
-
-# test on unseen data
-train_states = model.predict(test_X)
-y_pred=[]
-for i in xrange(len(X)):
-    state = train_states[i]
-    if state == 0:
-        y_pred.append(gp0.predict(X[i], return_std=True))
-    elif state == 1:
-        y_pred.append(gp1.predict(X[i], return_std=True))
-    elif state == 2:
-        y_pred.append(gp2.predict(X[i], return_std=True))
-    elif state == 3:
-        y_pred.append(gp3.predict(X[i], return_std=True))
-    elif state == 4:
-        y_pred.append(gp4.predict(X[i], return_std=True))
-    elif state == 5:
-        y_pred.append(gp5.predict(X[i], return_std=True))
-    elif state == 6:
-        y_pred.append(gp6.predict(X[i], return_std=True))
-
-preds = [x[0][0] for x in y_pred] 
-preds = np.array(preds)
-
-gp0 = models[0]
-gp0.score(train_X[np.where(train_states == 0)], train_Yshuffled[np.where(train_states == 0)])
-y0_pred = gp0.predict(train_X[np.where(train_states == 0)])
-plt.plot(Y[0:368], c = 'red')
-plt.plot(preds[0:368], c='blue')
+# Look at R-squared, rmse, and density plots of true vs predicted on train_X
+fig = plt.figure(figsize=(18, 15))
+for i in np.arange(1, 8):
+    r2_rmse(models[i], i)
+    print '-'*20
+    fig.add_subplot(4,2,i, title='Hidden state '+str(i))
+    sns.distplot(train_Y[np.where(train_hidden_states == i-1)], norm_hist=True, color='blue',label = 'true')
+    sns.distplot(models[i].predict(train_X[np.where(train_hidden_states == i-1)]), norm_hist=True,  color='red',label = 'pred')
+    plt.suptitle('Density of true vs. predicted values for each state on training data')
+    plt.legend(loc=1)
+    
 plt.show()
-for i in np.arange(7):
-    print models[i].score(train_X[np.where(train_states == i)], train_Yshuffled[np.where(train_states == i)])
 
-gp0 = gp.fit(train_X[np.where(train_states == 0)], train_Yshuffled[np.where(train_states == 0)])
+# predict the most likely state sequence for the unseen test data
+test_hidden_states = hmm.predict(test_X)
 
+# For each observation get the predicted hidden state and make a energy consumption prediction using the GP model for that state
+test_pred_hmm_gp = []
+test_std_hmm_gp = []
+for idx in range(len(test_X)):
+    state = test_hidden_states[idx]+1
+    pred, std = models[state].predict(test_X[idx], return_std=True)
+    test_pred_hmm_gp.append(pred)
+    test_std_hmm_gp.append(std)
+    
+test_std_hmm_gp = np.asarray(test_std_hmm_gp).flatten()
+test_pred_hmm_gp = np.asarray(test_pred_hmm_gp).flatten()
 
-
-
-
-
+# plot true vs predicted on the test_Y data
+plot_y([test_Y, test_pred_hmm_gp],  'True values vs. model predictions on unseen (test_X) data', 'Hours', 'Electric load (normalized)', ['Truth','Prediction'])
+in_ci = plot_CI('Confidence Interval on unseen (test_X) data', test_Y, test_pred_hmm_gp, test_std_hmm_gp, 95, 200, 325)
+print 'The percent of true values that fall within the confidence interval is ', 100*in_ci,'%'
